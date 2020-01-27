12968cae4b3e0905ffdd49a1162b220719f17610
Administrator <Administrator@172.18.0.28>
2020-01-27 11:04:55 +0000

    Adding Docker

--- dspcrowdcounter/src/main/java/com/dspcrowdcounter/WebSocketConfig.java
+++ dspcrowdcounter/src/main/java/com/dspcrowdcounter/WebSocketConfig.java
@@ -12,6 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry
+		  
    registry) {
     registry.addEndpoint("/mywebsockets")
         .setAllowedOrigins("mydomain.com").withSockJS();	
