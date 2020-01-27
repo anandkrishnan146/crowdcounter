2f61fc6d1c902e97dab1436c2024c433c6ca6622
Administrator <Administrator@172.18.0.28>
2020-01-27 07:24:51 +0000

    Streamer

--- dspcrowdcounter/src/main/java/com/dspcrowdcounter/rest/FaceDetectionController.java
+++ dspcrowdcounter/src/main/java/com/dspcrowdcounter/rest/FaceDetectionController.java
@@ -1,29 +1,40 @@
 package com.dspcrowdcounter.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dspcrowdcounter.OpenCVUtil.FaceDetectorVid;
-import com.dspcrowdcounter.OpenCVUtil.PeopleDetect;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class FaceDetectionController {
-	
-@Autowired 
-FaceDetectorVid FaceDetectorVid;
 
-
-
+	@Autowired
+	FaceDetectorVid FaceDetectorVid;
 
 	@RequestMapping("/crowdcounter/feed")
 	public String videoFeeder() {
-		//PeopleDetect peopleDetect = new PeopleDetect();
-		//peopleDetect.detect("C:\\Users\\Administrator\\Downloads\\videoplayback.mp4", "");
-		
+		// PeopleDetect peopleDetect = new PeopleDetect();
+		// peopleDetect.detect("C:\\Users\\Administrator\\Downloads\\videoplayback.mp4",
+		// "");
+
 		FaceDetectorVid.detectFace();
 		return "Feeding the live video to server";
 	}
+
+
+	 @MessageMapping("/news")
+	 @SendTo("/topic/news") 
+	 public String broadcastNews(@Payload String message) {
+	 return message;
+	 }
+	 
+	/*
+	 * @MessageMapping("/news") public void broadcastNews(@Payload String message) {
+	 * this.simpMessagingTemplate.convertAndSend("/topic/news", message) }
+	 */
 
 }
